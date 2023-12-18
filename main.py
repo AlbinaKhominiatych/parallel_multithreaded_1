8c4e1527bf7d549ebe224b06b34e6247a915b2de
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-12-18 20:04:19 +0200

    Створення демонстраційних потоків (для прикладу)

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 import threading
+from time import sleep
 
-# Створення демонстраційних потоків (для прикладу)
+counter = 0
 
-def worker():
-    print("Worker thread is executing")
 
-# Запуск декількох робітників
+def print_cube(num):
+    global counter
+    while True:
+        counter += 1
+        print(f'\ncounter {counter}\nCub:', num ** 3)
+        sleep(1.5)
 
-for i in range(5):
-    t = threading.Thread(target=worker)
-    t.start()
 
-# Вивід списку всіх живих потоків
+def print_square(num):
+    global counter
+    while True:
+        counter += 1
+        print(f'\ncounter {counter}\nSquare:', num ** 2)
+        sleep(1.8)
+
+
+t1 = threading.Thread(target=print_cube, args=(10,))
+t2 = threading.Thread(target=print_square, args=(10,))
+print('test')
+
+
+
+
+t1.start()
+t2.start()
+
+
+print('test2')
 
 current_threads = threading.enumerate()
- for thread in current_threads:
+for thread in current_threads:
     print(f"Thread Name: {thread.name}. Alive: {thread.is_alive()}")
+
+
+t1.join()
+t2.join()
+print('test2')
+print('end')
