c34bdfe8abdb499128c7173f5336e0d761f32889
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-12-18 19:32:53 +0200

    наглядний приклад на основі циклів

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import threading
+from time import sleep
+
+counter = 0
 
 def print_cube(num):
-    print("Куб:", num * num * num)
+    global counter
+    while True:
+        print(f'\ncounter {counter}\nCub:', num ** 3)
+        sleep(1)
+        counter += 1
+
 
 def print_square(num):
-    print("Квадрат:", num * num)
+    global counter
+    while True:
+        print(f'\ncounter {counter}\nSquare:', num ** 2)
+        sleep(1.2)
+        counter += 1
+
 
 t1 = threading.Thread(target=print_cube, args=(10,))
-t2 = threading.Thread(target=print_square, args=(10, ))
+t2 = threading.Thread(target=print_square, args=(10,))
+
 t1.start()
 t2.start()
+
 t1.join()
 t2.join()
-print("Готово!")
+print('end')
